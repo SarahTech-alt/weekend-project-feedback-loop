2416405301bde783dcd0b2913c0c475f66e2b329
Sarah Fuoss <sfuoss@gmail.com>
2021-09-23 21:54:54 -0500

    Added test post route from app.js to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pool = require('./modules/pool');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -11,6 +12,23 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 
 
+app.post( '/feedback', (req,res) =>{
+    console.log(req.body);
+    
+const newFeedback = req.body;
+const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+VALUES (4, 4, 5, 'Doing Great!');`
+pool.query(queryText).then(result => {
+    res.sendStatus(201);
+}).catch(error => {
+    console.log('There was an error making request', error);
+    
+    res.sendStatus(500);
+});
+})
+
+
+   
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,11 +10,30 @@ function App() {
     console.log('in useEffect');
   }, []);
 
+  const testRoute = () => {
+    axios({
+      method: 'POST',
+      url: '/feedback',
+      data: {
+        feeling: 1,
+        understanding: 1,
+        support: 1,
+        comments: 'none'
+      }
+    }).then(response =>{
+      console.log(response);}).catch(error => {
+        console.log('there was an error', error);
+      })
+  }
+
+  
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
+        <button onClick={testRoute}>Test</button>
       </header>
     </div>
   );
